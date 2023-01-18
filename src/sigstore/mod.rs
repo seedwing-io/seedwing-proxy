1e91f5c2974cec841638ae98e86b4beb28888ea8
Jim Crossley <jim@crossleys.org>
2023-01-18 17:20:32 -0500

    Factored the sigstore stuff out into its own module
    I did this to make it easier for the maven module to use it, but we
    may replace it entirely with -policy integration.
    
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- src/sigstore/mod.rs
+++ src/sigstore/mod.rs
@@ -1,1 +1,30 @@
+use sigstore::rekor::apis::{
+    configuration::Configuration,
+    entries_api::get_log_entry_by_uuid,
+    index_api::{search_index, SearchIndexError},
+    Error,
+};
+use sigstore::rekor::models::SearchIndex;
 
+pub async fn search(digest: String) -> Result<Vec<String>, Error<SearchIndexError>> {
+    let query = SearchIndex {
+        email: None,
+        public_key: None,
+        hash: Some(digest),
+    };
+    let configuration = Configuration::default();
+    match search_index(&configuration, query).await {
+        Ok(uuids) => {
+            if log::log_enabled!(log::Level::Debug) {
+                for uuid in uuids.iter() {
+                    let entry = get_log_entry_by_uuid(&configuration, uuid).await;
+                    if let Ok(entry) = entry {
+                        log::debug!("{:?}", entry);
+                    }
+                }
+            }
+            Ok(uuids)
+        }
+        Err(e) => Err(e),
+    }
+}
