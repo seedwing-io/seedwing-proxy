167aa10f4d75c9ce80e2ad3ab415e86a6018ad7a
Kevin Conner <kev.conner@gmail.com>
2023-02-06 12:26:33 -0800

    Add support for the sparse protocol

--- src/proxy/mod.rs
+++ src/proxy/mod.rs
@@ -1,19 +1,23 @@
 use url::Url;
 
-use super::repositories::crates::git;
 use crate::config::repositories::RepositoryType;
 use crate::config::Config;
 use crate::policy::PolicyEngine;
 use crate::repositories::crates::git::IndexRepository;
+use crate::repositories::crates::sparse::SparseRepository;
 use crate::{repositories, ui};
 use actix_web::middleware::Logger;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
+
+const INDEX_PATH: &str = "/index";
+const API_PATH: &str = "/api/v1";
 
 pub struct Proxy {
     config: Config,
     crate_repositories: HashMap<String, IndexRepository>,
+    crate_sparse_repositories: HashMap<String, SparseRepository>,
 }
 
 impl Proxy {
@@ -21,6 +25,7 @@ impl Proxy {
         Self {
             config,
             crate_repositories: HashMap::new(),
+            crate_sparse_repositories: HashMap::new(),
         }
     }
 
@@ -43,43 +48,86 @@ impl Proxy {
         let base_cache_dir = self.config.proxy().expanded_cache_dir();
 
         for (scope, config) in self.config.repositories().iter() {
-            if RepositoryType::Crates == config.repository_type() {
-                log::info!(
-                    "------------------------------------------------------------------------"
-                );
-                log::info!("Initialising Crate Repository for scope {scope}");
-                let index_repository = git::IndexRepository::new(
-                    config.url(),
-                    self.get_cache_dir(&base_cache_dir, scope, &bind_args.0, bind_args.1),
-                    self.get_url(scope, &bind_args.0, bind_args.1, Some("/api/v1/crates")),
-                    self.get_url(scope, &bind_args.0, bind_args.1, None),
-                    config.periodic_update(),
-                );
-                log::info!(
-                    "    Crate repository       : {}",
-                    index_repository.get_repo()
-                );
-                log::info!(
-                    "    local repository cache : {}",
-                    index_repository.get_local_repository_cache().display()
-                );
-                log::info!(
-                    "    download URL           : {}",
-                    index_repository.get_dl_url()
-                );
-                log::info!(
-                    "    API URL                : {}",
-                    index_repository.get_api_url()
-                );
-                log::info!(
-                    "    Periodic Update        : {}",
-                    index_repository.get_periodic_update()
-                );
-                self.crate_repositories
-                    .insert(scope.to_string(), index_repository);
-                log::info!(
-                    "------------------------------------------------------------------------"
-                );
+            match config.repository_type() {
+                RepositoryType::Crates => {
+                    log::info!(
+                        "------------------------------------------------------------------------"
+                    );
+                    log::info!("Initialising Crate Repository for scope {scope}");
+                    let index_repository = IndexRepository::new(
+                        config.url(),
+                        self.get_cache_dir(&base_cache_dir, scope, &bind_args.0, bind_args.1),
+                        self.get_url(
+                            scope,
+                            &bind_args.0,
+                            bind_args.1,
+                            Some(&format!("{API_PATH}/crates")),
+                        ),
+                        self.get_url(scope, &bind_args.0, bind_args.1, None),
+                        config.periodic_update(),
+                    );
+                    log::info!(
+                        "    Crate repository       : {}",
+                        index_repository.get_repo()
+                    );
+                    log::info!(
+                        "    local repository cache : {}",
+                        index_repository.get_local_repository_cache().display()
+                    );
+                    log::info!(
+                        "    download URL           : {}",
+                        index_repository.get_dl_url()
+                    );
+                    log::info!(
+                        "    API URL                : {}",
+                        index_repository.get_api_url()
+                    );
+                    log::info!(
+                        "    Periodic Update        : {}",
+                        index_repository.get_periodic_update()
+                    );
+                    self.crate_repositories
+                        .insert(scope.to_string(), index_repository);
+                    log::info!(
+                        "------------------------------------------------------------------------"
+                    );
+                }
+                RepositoryType::SparseCrates => {
+                    log::info!("Initialising Crate Sparse Repository for scope {scope}");
+                    let sparse_repository = SparseRepository::new(
+                        config.url(),
+                        format!("/{scope}{INDEX_PATH}"),
+                        self.get_url(
+                            scope,
+                            &bind_args.0,
+                            bind_args.1,
+                            Some(&format!("{API_PATH}/crates")),
+                        ),
+                        self.get_url(scope, &bind_args.0, bind_args.1, None),
+                    );
+                    log::info!(
+                        "    Crate sparse repository: {}",
+                        sparse_repository.get_repo()
+                    );
+                    log::info!(
+                        "    download URL           : {}",
+                        sparse_repository.get_dl_url()
+                    );
+                    log::info!(
+                        "    API URL                : {}",
+                        sparse_repository.get_api_url()
+                    );
+                    log::info!(
+                        "    Index Prefix           : {}",
+                        sparse_repository.get_index_prefix()
+                    );
+                    self.crate_sparse_repositories
+                        .insert(scope.to_string(), sparse_repository);
+                    log::info!(
+                        "------------------------------------------------------------------------"
+                    );
+                }
+                RepositoryType::M2 => {}
             }
         }
 
@@ -94,17 +142,25 @@ impl Proxy {
             for service in self.config.repositories().iter().map(|(scope, config)| {
                 match config.repository_type() {
                     RepositoryType::Crates => {
-                        if let Some(index_repository) = self.crate_repositories.get(scope) {
-                            let git_cmd = &self.config.proxy().git_cmd();
-                            repositories::crates::service(scope, git_cmd, index_repository.clone())
-                        } else {
-                            log::info!(
-                                "Ignoring scope {scope} because of earlier initialisation failures"
-                            );
-                            web::scope(scope).default_service(web::to(HttpResponse::NotFound))
-                        }
+                        let index_repository = self.crate_repositories.get(scope).unwrap();
+                        let git_cmd = &self.config.proxy().git_cmd();
+                        repositories::crates::service(
+                            scope,
+                            git_cmd,
+                            index_repository.clone(),
+                            API_PATH,
+                        )
                     }
                     RepositoryType::M2 => repositories::maven::service(scope, config.url()),
+                    RepositoryType::SparseCrates => {
+                        let sparse_repository = self.crate_sparse_repositories.get(scope).unwrap();
+                        repositories::crates::service_sparse(
+                            scope,
+                            sparse_repository.clone(),
+                            INDEX_PATH,
+                            API_PATH,
+                        )
+                    }
                 }
             }) {
                 app = app.service(service)
