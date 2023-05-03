84b9fdf41a384b58cdc94c7fd6706bee2c4a2eb2
Jim Crossley <jim@crossleys.org>
2023-05-03 13:34:25 -0400

    Don't uncompress upstream response before returning it
    Signed-off-by: Jim Crossley <jim@crossleys.org>

--- src/repositories/pip/mod.rs
+++ src/repositories/pip/mod.rs
@@ -1,6 +1,4 @@
-use actix_web::{
-    route, web, Error, HttpRequest, HttpResponse, HttpResponseBuilder, Responder, Scope,
-};
+use actix_web::{route, web, HttpRequest, HttpResponse, HttpResponseBuilder, Responder, Scope};
 use url::Url;
 
 use crate::policy::PolicyEngine;
@@ -27,33 +25,30 @@ pub fn service(scope: &str, url: Url) -> Scope {
         .service(pass_through)
 }
 
-#[route("{any:.*}", method = "GET", method = "HEAD")]
+#[route("{any:.*}", method = "GET", method = "HEAD", method = "POST")]
 async fn pass_through(
     req: HttpRequest,
+    payload: web::Payload,
     config: web::Data<PipConfig>,
     policy: web::Data<PolicyEngine>,
     path: web::Path<String>,
 ) -> impl Responder {
     let path = path.into_inner();
-    let uri = format!("{}{path}", config.url,);
+    let uri = format!("{}{path}", config.url);
     log::debug!("pass: {uri}");
-    let request = policy.client.request_from(&uri, req.head());
+    let request = policy.client.request_from(uri, req.head()).no_decompress();
     log::debug!("request: {request:?}");
-    match request.send().await {
-        Ok(mut upstream) => match upstream.body().limit(20_000_000).await {
-            Ok(payload) => {
-                let mut response = HttpResponseBuilder::new(upstream.status());
-                for header in upstream.headers().iter() {
-                    response.insert_header(header);
-                }
-                response.body(payload)
+    match request.send_stream(payload).await {
+        Ok(upstream) => {
+            let mut response = HttpResponseBuilder::new(upstream.status());
+            for header in upstream.headers().iter() {
+                response.insert_header(header);
             }
-            Err(e) => Error::from(e).into(),
-        },
+            response.streaming(upstream)
+        }
         Err(e) => {
-            let msg = format!("Error encountered proxying {uri} -> {e}");
-            log::error!("{msg}");
-            HttpResponse::InternalServerError().body(msg)
+            log::error!("proxy error: {}", e);
+            HttpResponse::NotFound().body("not found")
         }
     }
 }
